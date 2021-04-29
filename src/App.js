7f0f3fe5c21c4d49b0310903411d376d793d115b
codingbrowny <codingbrowny@gmail.com>
2021-04-29 02:50:33 +0000

    My portfolio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <a className="portfolio-url" href="https://www.codingbrowny.xyz">Codingbrowny</a>
         </p>
         <a
           className="App-link"
