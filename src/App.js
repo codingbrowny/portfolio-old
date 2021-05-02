d8709badfc39da28cbee388568dbc498243b1611
codingbrowny <codingbrowny@gmail.com>
2021-05-02 09:58:46 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Navigationbar from "./components/ui/navbar/nav";
-import Hero from './components/ui/hero/hero';
-import About from './components/ui/about/about'
+import Navigationbar from './components/ui/navbar/nav'
+import Hero from './components/ui/hero/hero'
+import About from './components/ui/about/about';
 
 function App() {
   return (
